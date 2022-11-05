46527ce93537afb249d19c3c22ea97be6059cf88
Nappy97 <wjddmlgud829@gmail.com>
2022-11-05 23:26:46 +0900

    1105nappy

---
+++ src/main/java/org/example/java_programming_language/collectionFramework2.java
@@ -0,0 +1,11 @@
+package org.example.java_programming_language;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class collectionFramework2 {
+
+    public static void main(String[] args) {
+        List<java.lang.Object> myList1 = new ArrayList<>();
+    }
+}
