c2f03436d17664aedffba7dd00a1805f2a7a21d7
Nappy97 <wjddmlgud829@gmail.com>
2022-11-08 23:17:19 +0900

    1108

---
+++ src/main/java/org/example/java_programming_language/ThreadClassEx.java
@@ -0,0 +1,15 @@
+package org.example.java_programming_language;
+
+class MyThread extends Thread {
+    public void run() {
+        /* 작업 내용 */     // Thread클래스의 run()을 오버라이딩
+    }
+}
+
+class MyThread1 implements Runnable {
+    public void run() {
+        /*작업내용*/    // Runnable인터페이스의 run()을 구현
+    }
+}
+
+
